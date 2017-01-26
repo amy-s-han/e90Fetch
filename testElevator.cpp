e028f5cec66cadf0a77ba27cb54a9c099be0e909
Amy Han <ahan2@swarthmore.edu>
2017-01-26 17:33:41 -0500

    detected 1 circle

--- testElevator.cpp
+++ testElevator.cpp
@@ -8,10 +8,13 @@ using namespace cv;
 /** @function main */
 int main(int argc, char** argv)
 {
-  Mat src, src_gray;
+  Mat inSrc, src, src_gray;
 
   /// Read the image
-  src = imread( argv[1], 1 );
+  // inSrc = imread( argv[1], 1 );
+  inSrc = imread("inside3.jpg", 1 );
+
+  cv::resize(inSrc, src, cv::Size(), 0.25, 0.25);
 
   if( !src.data )
     { return -1; }
@@ -19,13 +22,23 @@ int main(int argc, char** argv)
   /// Convert it to gray
   cvtColor( src, src_gray, CV_BGR2GRAY );
 
+  // namedWindow( "gray", CV_WINDOW_AUTOSIZE );
+  // imshow( "gray", src_gray );
+
   /// Reduce the noise so we avoid false circle detection
   GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2 );
 
+
+  waitKey(0);
+
   vector<Vec3f> circles;
 
+  std::cout << "rows: " << src_gray.rows << ". rows divided: " << src_gray.rows/90 << std::endl;
+
   /// Apply the Hough Transform to find the circles
-  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 100, 0, 0 );
+  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/90, 200, 100, 0, 0 );
+
+  std::cout << "There are " << circles.size() << "." << std::endl;
 
   /// Draw the circles detected
   for( size_t i = 0; i < circles.size(); i++ )
