4c5d669df967b2d53db06912258ae2ab7b03f17d
Amy Han <ahan2@swarthmore.edu>
2017-03-25 03:53:10 -0400

    lordy now I have linker errors

--- my_awesome_package/src/monkeys.cpp
+++
@@ -1,5 +0,0 @@
-#include "monkeys.h"
-
-std::string get_monkeys() {
-  return "it's the monkeys!";
-}
--- my_awesome_package/src/monkeys.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MONKEYS_H_
-#define _MONKEYS_H_
-
-#include <string>
-
-std::string get_monkeys();
-
-#endif
